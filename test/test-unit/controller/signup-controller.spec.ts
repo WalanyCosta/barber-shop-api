41588441dfbec3790f587f24a4e2d799e05283f8
Walany Costa <walanybnegro@gmail.com>
2023-12-04 18:21:43 +0100

    test: ensure SignUpController returns 200 on success

--- test/test-unit/controller/signup-controller.spec.ts
+++ test/test-unit/controller/signup-controller.spec.ts
@@ -208,4 +208,13 @@ describe('SignUp Controller', () => {
       body: error
     })
   })
+
+  test('should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(fakeHttpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: 'any_token'
+    })
+  })
 })
