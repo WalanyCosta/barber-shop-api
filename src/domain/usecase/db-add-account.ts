94d4f16329b7be15792c5f8ff48011231af6a048
Walany Costa <walanybnegro@gmail.com>
2023-12-05 14:03:19 +0100

    feat: ensure DbAddAccount calls Hasher with correct param

--- src/domain/usecase/db-add-account.ts
+++ src/domain/usecase/db-add-account.ts
@@ -1,12 +1,18 @@
 import { type AddAccountParam, type AddAccountRepository } from '../protocols/add-account'
+import { type Hasher } from '../protocols/hasher'
 import { type LoadAccountByEmailRepository } from '../protocols/load-account-by-email-repository'
 
 export class DbAddAccount implements AddAccountRepository {
-  constructor (private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+  constructor (
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hasher: Hasher) {}
 
   async add (addAccountParam: AddAccountParam): Promise<string | null> {
     const response = await this.loadAccountByEmailRepository.load(addAccountParam.email)
-    if (!response) return 'any_token'
+    if (!response) {
+      await this.hasher.hash(addAccountParam.password)
+      return 'any_token'
+    }
     return null
   }
 }
