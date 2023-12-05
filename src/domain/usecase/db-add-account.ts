9ecdc0a90014a79df8deb629c7892b1f76dfbb4e
Walany Costa <walanybnegro@gmail.com>
2023-12-05 18:56:14 +0100

    feat: ensure DbAddAccount calls Encrypter with correct param

--- src/domain/usecase/db-add-account.ts
+++ src/domain/usecase/db-add-account.ts
@@ -1,5 +1,6 @@
 import { type AddAccountParam, type AddAccount } from '../protocols/add-account'
 import { type AddAccountRepository } from '../protocols/add-account-repository'
+import { type Encrypter } from '../protocols/encrypter'
 import { type Hasher } from '../protocols/hasher'
 import { type LoadAccountByEmailRepository } from '../protocols/load-account-by-email-repository'
 
@@ -7,17 +8,20 @@ export class DbAddAccount implements AddAccount {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly encrypter: Encrypter
   ) {}
 
   async add (addAccountParam: AddAccountParam): Promise<string | null> {
     const account = await this.loadAccountByEmailRepository.load(addAccountParam.email)
     if (!account) {
       const hashed = await this.hasher.hash(addAccountParam.password)
-      await this.addAccountRepository.add({
+      const newAccount = await this.addAccountRepository.add({
         ...addAccountParam,
         password: hashed
       })
+
+      await this.encrypter.encrypt(newAccount.id)
       return 'any_token'
     }
     return null
