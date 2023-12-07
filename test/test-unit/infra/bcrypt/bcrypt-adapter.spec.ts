8194727c370c263de1bb5bc60576d21532962260
Walany Costa <walanybnegro@gmail.com>
2023-12-07 02:26:27 +0100

    refactor: change sut to creation factory method

--- test/test-unit/infra/bcrypt/bcrypt-adapter.spec.ts
+++ test/test-unit/infra/bcrypt/bcrypt-adapter.spec.ts
@@ -7,18 +7,22 @@ jest.mock('bcrypt', () => ({
   }
 }))
 
+const makeSut = (salt = 10): BcryptAdapter => {
+  const sut = new BcryptAdapter(salt)
+  return sut
+}
+
 describe('Bcrypt Adapter', () => {
   test('should call hash with correct value', async () => {
     const saltRounds = 10
-    const sut = new BcryptAdapter(saltRounds)
+    const sut = makeSut(saltRounds)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', saltRounds)
   })
 
   test('should throw if hash throws', async () => {
-    const saltRounds = 10
-    const sut = new BcryptAdapter(saltRounds)
+    const sut = makeSut()
     const mockedHash = bcrypt.hash as jest.Mock<any, any>
     mockedHash.mockRejectedValueOnce(new Error())
     const error = sut.hash('any_value')
