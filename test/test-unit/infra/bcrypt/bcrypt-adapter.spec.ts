e3e16a92f1707bc43342bc52fbda460614eea279
Walany Costa <walanybnegro@gmail.com>
2023-12-07 02:18:45 +0100

    test: ensure BcrypterAdapter throws if hash throws

--- test/test-unit/infra/bcrypt/bcrypt-adapter.spec.ts
+++ test/test-unit/infra/bcrypt/bcrypt-adapter.spec.ts
@@ -15,4 +15,13 @@ describe('Bcrypt Adapter', () => {
     await sut.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', saltRounds)
   })
+
+  test('should throw if hash throws', async () => {
+    const saltRounds = 10
+    const sut = new BcryptAdapter(saltRounds)
+    const mockedHash = bcrypt.hash as jest.Mock<any, any>
+    mockedHash.mockRejectedValueOnce(new Error())
+    const error = sut.hash('any_value')
+    await expect(error).rejects.toThrow()
+  })
 })
