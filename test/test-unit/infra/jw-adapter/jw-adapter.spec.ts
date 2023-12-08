073659608505b5d3073b899c94708a7582f6655a
Walany Costa <walanybnegro@gmail.com>
2023-12-08 02:29:10 +0100

    test: ensure JwtAdapter calls sign with correct param

---
+++ test/test-unit/infra/jw-adapter/jw-adapter.spec.ts
@@ -0,0 +1,18 @@
+import { JwtAdapter } from './../../../../src/infra/jw-adapter/jwt-adapter'
+import jwt from 'jsonwebtoken'
+
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return await Promise.resolve('any_token')
+  }
+}))
+
+describe('JWT Adapter', () => {
+  test('should call sign with correct param', async () => {
+    const privatekey = 'secret'
+    const sut = new JwtAdapter(privatekey)
+    const signSpy = jest.spyOn(jwt, 'sign')
+    await sut.encrypt('any_id')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, privatekey)
+  })
+})
