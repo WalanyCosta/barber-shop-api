2ed0421a7097d4110915ffae3065d240f7591053
Walany Costa <walanybnegro@gmail.com>
2023-12-15 00:08:37 +0100

    feat: ensure AccountRepository returns an account on load success

--- src/infra/db/prisma/account/account-repository.ts
+++ src/infra/db/prisma/account/account-repository.ts
@@ -1,11 +1,21 @@
+import { type LoadAccountByEmailRepository } from './../../../../domain/protocols/load-account-by-email-repository'
 import { type AccountModel } from '../../../../domain/model/account-model'
 import { type AddAccountModel, type AddAccountRepository } from '../../../../domain/protocols/add-account-repository'
 import prisma from '../helpers/client'
 
-export class AccountRepository implements AddAccountRepository {
+export class AccountRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (addAccountModel: AddAccountModel): Promise<AccountModel> {
     const account = await prisma.account.create({
       data: addAccountModel
+    })
+    return account as AccountModel
+  }
+
+  async load (email: string): Promise<AccountModel | null> {
+    const account = await prisma.account.findFirst({
+      where: {
+        email
+      }
     })
     return account as AccountModel
   }
