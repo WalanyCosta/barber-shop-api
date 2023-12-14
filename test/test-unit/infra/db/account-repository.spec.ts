a7598120c1c1095267b4a6fce096e42bbf8522c2
Walany Costa <walanybnegro@gmail.com>
2023-12-14 23:40:31 +0100

    test: ensure AccountRepository returns an account on add success

---
+++ test/test-unit/infra/db/account-repository.spec.ts
@@ -0,0 +1,32 @@
+import { AccountRepository } from './../../../../src/infra/db/prisma/account/account-repository'
+import { prismaMock } from './../../../../src/infra/db/prisma/helpers/singleton'
+import { type AccountModel } from '../../../../src/domain/model/account-model'
+
+const fakeAccount: AccountModel = ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password',
+  phone: 'any_phone',
+  avatar: '',
+  accessToken: ''
+})
+
+describe('AccountRepository', () => {
+  test('should return an account on add success', async () => {
+    const sut = new AccountRepository()
+    prismaMock.account.create.mockResolvedValueOnce(fakeAccount)
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+      phone: 'any_phone'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+    expect(account.phone).toBe('any_phone')
+  })
+})
