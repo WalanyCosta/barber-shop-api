e940aee0f5cd85edc3df163a9975f7b3fd2dbe5b
Walany Costa <walanybnegro@gmail.com>
2023-12-17 13:38:54 +0100

    feat: ensure ZodValidator returns error if params not follow rules in schema

--- src/infra/validator/schema/signup-schema.ts
+++ src/infra/validator/schema/signup-schema.ts
@@ -7,4 +7,14 @@ export const signupSchema = z.object({
     .min(1, 'email is empty. Please write email')
     .email('email is invalid. Please write email correctly'),
   password: z.string({ required_error: 'password is required. Please write password' })
+    .regex(new RegExp('.*[A-Z].*'), 'Password is weak. Please write password')
+    .regex(new RegExp('.*[a-z].*'), 'Password is weak. Please write password')
+    .regex(new RegExp('.*\\d.*'), 'Password is weak. Please write password')
+    .regex(
+      new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),
+      'Password is weak. Please write password'
+    )
+    .min(8, 'Password must be at least 8 characters in length'),
+  phone: z.string({ required_error: 'phone is required. Please write phone' })
+    .min(1, 'phone is empty. Please write phone')
 })
