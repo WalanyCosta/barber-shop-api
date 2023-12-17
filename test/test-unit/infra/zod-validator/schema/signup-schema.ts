b3ee18a190389cd9ba2e2f367a794292d51d29af
Walany Costa <walanybnegro@gmail.com>
2023-12-17 12:49:06 +0100

    feat: ensure ZodValidation returns error if name is empty

--- test/test-unit/infra/zod-validator/schema/signup-schema.ts
+++ test/test-unit/infra/zod-validator/schema/signup-schema.ts
@@ -2,4 +2,5 @@ import * as z from 'zod'
 
 export const signupSchema = z.object({
   name: z.string({ required_error: 'name is required. Please write name' })
+    .min(1, 'name is empty. Please write name')
 })
