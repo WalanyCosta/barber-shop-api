6854fec02fc6a7da905c93aa0384d25b25b1f22e
Walany Costa <walanybnegro@gmail.com>
2023-12-17 12:54:29 +0100

    feat: ensure ZodValidator returns error if email is required

--- src/infra/validator/schema/signup-schema.ts
+++ src/infra/validator/schema/signup-schema.ts
@@ -2,5 +2,6 @@ import * as z from 'zod'
 
 export const signupSchema = z.object({
   name: z.string({ required_error: 'name is required. Please write name' })
-    .min(1, 'name is empty. Please write name')
+    .min(1, 'name is empty. Please write name'),
+  email: z.string({ required_error: 'email is required. Please write email' })
 })
