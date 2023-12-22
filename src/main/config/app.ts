182306f4574250e2eb5175122e5cab181de220d9
Walany Costa <walanybnegro@gmail.com>
2023-12-22 05:20:55 +0100

    feat: ensure request parse body as json

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { cors } from '../middlewares/cors'
+import { bodyParser } from '../middlewares/body-parser'
 
 const app = express()
 
 app.use(cors)
+app.use(bodyParser)
 
 export default app
