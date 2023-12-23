842f361c4435d3de9b1fb4cd80d8f07ab91161fa
Walany Costa <walanybnegro@gmail.com>
2023-12-24 00:59:13 +0100

    feat: connect to prisma before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,10 @@
 import 'dotenv/config'
+import prisma from '../infra/db/prisma/helpers/client'
 
-(async () => {
-  const port = process.env.PORT
+const port = process.env.PORT
+prisma.$connect().then(async () => {
   const app = (await import('./config/app')).default
   app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
   })
-})()
+}).catch((error) => { console.log(error) })
