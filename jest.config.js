2d95aef8968e30a56fd7550232a1e69b83523069
Walany Costa <walanybnegro@gmail.com>
2023-12-24 01:33:59 +0100

    chore: restrict coverage to 100 percents only

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,14 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
   ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   preset: 'ts-jest',
   testEnvironment: 'node',
 };
