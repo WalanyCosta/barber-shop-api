963b1243fd3894ad80dd47eec6e29708e39b480a
WalanyCosta <walanybnegro@gmail.com>
2024-03-02 02:24:56 +0100

    test: ensure LoginController calls validate with correct params

---
+++ test/test-unit/controller/login-controller.spec.ts
@@ -0,0 +1,28 @@
+import { LoginController } from './../../../src/presentation/controller/login/login-controller'
+import { type Validator } from '../../../src/presentation/protocols/validator'
+
+const makeValidator = (): Validator => {
+  class ValidatorStub implements Validator {
+    validate (input: any): Error | null {
+      return null
+    }
+  }
+
+  return new ValidatorStub()
+}
+
+describe('Login Controller', () => {
+  test('should call validate with correct params', async () => {
+    const validatorStub = makeValidator()
+    const sut = new LoginController(validatorStub)
+    const validateSpy = jest.spyOn(validatorStub, 'validate')
+    const fakeHttpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    await sut.handle(fakeHttpRequest)
+    expect(validateSpy).toHaveBeenCalledWith(fakeHttpRequest.body)
+  })
+})
