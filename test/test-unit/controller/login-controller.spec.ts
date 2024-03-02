7b45fc61bcc39498a168724e26d495215ee2061a
WalanyCosta <walanybnegro@gmail.com>
2024-03-02 02:41:18 +0100

    test: ensure LoginController returns status 400 if validate throws error

--- test/test-unit/controller/login-controller.spec.ts
+++ test/test-unit/controller/login-controller.spec.ts
@@ -25,4 +25,22 @@ describe('Login Controller', () => {
     await sut.handle(fakeHttpRequest)
     expect(validateSpy).toHaveBeenCalledWith(fakeHttpRequest.body)
   })
+
+  test('should return 400 if validate throws error', async () => {
+    const validatorStub = makeValidator()
+    const sut = new LoginController(validatorStub)
+    const error = new Error()
+    jest.spyOn(validatorStub, 'validate').mockReturnValue(error)
+    const fakeHttpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(fakeHttpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      body: error
+    })
+  })
 })
