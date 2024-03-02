d1073da8aaf21b2577e49efc0753be2908915719
WalanyCosta <walanybnegro@gmail.com>
2024-03-02 02:52:30 +0100

    refactor: change sut to creation factory method

--- test/test-unit/controller/login-controller.spec.ts
+++ test/test-unit/controller/login-controller.spec.ts
@@ -1,6 +1,28 @@
 import { LoginController } from './../../../src/presentation/controller/login/login-controller'
 import { type Validator } from '../../../src/presentation/protocols/validator'
 
+interface SutTypes {
+  sut: LoginController
+  validatorStub: Validator
+}
+
+const makeSut = (): SutTypes => {
+  const validatorStub = makeValidator()
+  const sut = new LoginController(validatorStub)
+
+  return {
+    sut,
+    validatorStub
+  }
+}
+
+const fakeHttpRequest = {
+  body: {
+    email: 'any_email@mail.com',
+    password: 'any_password'
+  }
+}
+
 const makeValidator = (): Validator => {
   class ValidatorStub implements Validator {
     validate (input: any): Error | null {
@@ -13,22 +35,14 @@ const makeValidator = (): Validator => {
 
 describe('Login Controller', () => {
   test('should call validate with correct params', async () => {
-    const validatorStub = makeValidator()
-    const sut = new LoginController(validatorStub)
+    const { sut, validatorStub } = makeSut()
     const validateSpy = jest.spyOn(validatorStub, 'validate')
-    const fakeHttpRequest = {
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_password'
-      }
-    }
     await sut.handle(fakeHttpRequest)
     expect(validateSpy).toHaveBeenCalledWith(fakeHttpRequest.body)
   })
 
   test('should return 400 if validate throws error', async () => {
-    const validatorStub = makeValidator()
-    const sut = new LoginController(validatorStub)
+    const { sut, validatorStub } = makeSut()
     const error = new Error()
     jest.spyOn(validatorStub, 'validate').mockReturnValue(error)
     const fakeHttpRequest = {
