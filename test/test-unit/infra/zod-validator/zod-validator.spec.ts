2e538e3c55e23161f4a6bdd1a34579d15ca65e8d
WalanyCosta <walanybnegro@gmail.com>
2024-03-03 23:18:59 +0100

    test: ensure zodValidator is validate email with schema of the login

--- test/test-unit/infra/zod-validator/zod-validator.spec.ts
+++ test/test-unit/infra/zod-validator/zod-validator.spec.ts
@@ -1,8 +1,9 @@
+import { LoginSchema } from './../../../../src/infra/validator/schema/login-schema'
 import { ValidationError } from './../../../../src/presentation/errors/validation-error'
 import { ZodValidator } from '../../../../src/infra/validator/zod-validator'
 import { signupSchema } from '../../../../src/infra/validator/schema/signup-schema'
 
-describe('ZodValidator', () => {
+describe('ZodValidator - signup', () => {
   test('should return error if name is required', () => {
     const sut = new ZodValidator(signupSchema)
     const error = sut.validate({})
@@ -92,5 +93,34 @@ describe('ZodValidator', () => {
       phone: ''
     })
     expect(error).toEqual(new ValidationError('phone is empty. Please write phone'))
+  })
+})
+
+describe('ZodValidator-Login', () => {
+  test('should return error if email is required', () => {
+    const sut = new ZodValidator(LoginSchema)
+    const error = sut.validate({
+      name: 'any_name'
+    })
+    expect(error).toEqual(new ValidationError('email is required. Please write email'))
+  })
+
+  test('should return error if email is empty', () => {
+    const sut = new ZodValidator(LoginSchema)
+    const error = sut.validate({
+      name: 'any_name',
+      email: ''
+    })
+    expect(error).toEqual(new ValidationError('email is empty. Please write email'))
+  })
+
+  test('should return error if email is invalid', () => {
+    const sut = new ZodValidator(LoginSchema)
+    const error = sut.validate({
+      name: 'any_name',
+      email: 'any_email'
+    })
+    expect(error).toEqual(new ValidationError(
+      'email is invalid. Please write email correctly'))
   })
 })
