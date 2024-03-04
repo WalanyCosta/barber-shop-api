ac701a17e59de2ba1685d7b26cc005d086267740
WalanyCosta <walanybnegro@gmail.com>
2024-03-04 23:08:59 +0100

    feat: ensure DbAuthentication calls hashComparer with correct params

--- src/domain/usecase/db-authentication.ts
+++ src/domain/usecase/db-authentication.ts
@@ -1,14 +1,21 @@
 import { UnauthorizedError } from '../../presentation/errors/unauthorized-error'
+import { type HashComparer } from '../protocols/infra/crypto/hash-comparer'
 import { type LoadAccountByEmailRepository } from '../protocols/infra/db/load-account-by-email-repository'
 import { type Authentication, type AuthenticationParam } from '../protocols/presentation/authentication'
 
 export class DbAuthentication implements Authentication {
-  constructor (private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+  constructor (
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashComparer: HashComparer
+  ) {}
+
   async auth (param: AuthenticationParam): Promise<string | null> {
     const account = await this.loadAccountByEmailRepository.load(param.email)
     if (!account) {
       throw new UnauthorizedError('user not exists')
     }
+    await this.hashComparer.compare(param.password, account.password)
+
     return await Promise.resolve('any_token')
   }
 }
