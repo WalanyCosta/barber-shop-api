1fb46a471d3ee3fe8cbfae2d9e0e3081f2bb93b2
WalanyCosta <walanybnegro@gmail.com>
2024-03-04 02:03:06 +0100

    feat: ensure DbAuthentication throws if loadAccountByEmailRepository returns null

--- src/domain/usecase/db-authentication.ts
+++ src/domain/usecase/db-authentication.ts
@@ -1,10 +1,14 @@
+import { UnauthorizedError } from '../../presentation/errors/unauthorized-error'
 import { type LoadAccountByEmailRepository } from '../protocols/infra/db/load-account-by-email-repository'
 import { type Authentication, type AuthenticationParam } from '../protocols/presentation/authentication'
 
 export class DbAuthentication implements Authentication {
   constructor (private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
   async auth (param: AuthenticationParam): Promise<string | null> {
-    this.loadAccountByEmailRepository.load(param.email)
-    return await Promise.resolve('any')
+    const account = await this.loadAccountByEmailRepository.load(param.email)
+    if (!account) {
+      throw new UnauthorizedError('user not exists')
+    }
+    return await Promise.resolve('any_token')
   }
 }
