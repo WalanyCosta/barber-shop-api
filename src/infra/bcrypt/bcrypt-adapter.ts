bac6a34c036e66c27ed7e69dadc46e452ae13b6c
WalanyCosta <walanybnegro@gmail.com>
2024-03-05 23:32:45 +0100

    feat: ensure BcryptAdapter calls compare with correct params

--- src/infra/bcrypt/bcrypt-adapter.ts
+++ src/infra/bcrypt/bcrypt-adapter.ts
@@ -1,8 +1,14 @@
+import { type HashComparer } from './../../domain/protocols/infra/crypto/hash-comparer'
 import { type Hasher } from '../../domain/protocols/infra/crypto/hasher'
-import { hash } from 'bcrypt'
+import { hash, compare } from 'bcrypt'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
   constructor (private readonly saltRounds: number) {}
+
+  async compare (value: string, hash: string): Promise<boolean> {
+    await compare(value, hash)
+    return await Promise.resolve(false)
+  }
 
   async hash (value: string): Promise<string> {
     const response = await hash(value, this.saltRounds)
