fd93048834f6a30f49b646ac6c11857a2b9cc71c
WalanyCosta <walanybnegro@gmail.com>
2024-03-07 03:03:16 +0100

    test: ensure DbLoadServices calls LoadServicesRepository

---
+++ test/test-unit/domain/usecase/db-load-services.spec.ts
@@ -0,0 +1,42 @@
+import { DbLoadServices } from './../../../../src/domain/usecase/load-services-repository'
+import { type LoadServicesRepository } from './../../../../src/domain/protocols/infra/db/load-service-repository'
+import { StatusService, type ServiceModel } from '../../../../src/domain/model/service-model'
+
+const fakeServicesResponse = ([
+  {
+    id: 'any_id',
+    name: 'any_name',
+    price: 30,
+    star: 5,
+    status: StatusService.active,
+    category: 'any_category'
+  },
+  {
+    id: 'other_id',
+    name: 'other_name',
+    price: 20,
+    star: 3,
+    status: StatusService.disable,
+    category: 'other_category'
+  }
+])
+
+const makeLoadServicesRepository = (): LoadServicesRepository => {
+  class LoadServicesRepositoryStub implements LoadServicesRepository {
+    async load (): Promise<ServiceModel[]> {
+      return await Promise.resolve(fakeServicesResponse)
+    }
+  }
+
+  return new LoadServicesRepositoryStub()
+}
+
+describe('DbLoadServices', () => {
+  test('should call LoadServicesRepository', async () => {
+    const loadServicesRepositoryStub = makeLoadServicesRepository()
+    const sut = new DbLoadServices(loadServicesRepositoryStub)
+    const loadSpy = jest.spyOn(loadServicesRepositoryStub, 'load')
+    await sut.load()
+    expect(loadSpy).toHaveBeenCalled()
+  })
+})
