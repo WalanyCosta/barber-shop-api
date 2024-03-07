6b4b76f6dcefe4c92dc2167a59598988d3cf4e71
WalanyCosta <walanybnegro@gmail.com>
2024-03-07 02:14:24 +0100

    refactor: move SUT to creation factory method

--- test/test-unit/controller/load-services-controller.spec.ts
+++ test/test-unit/controller/load-services-controller.spec.ts
@@ -2,31 +2,48 @@ import { LoadServicesController } from './../../../src/presentation/controller/l
 import { type LoadServices } from './../../../src/domain/protocols/presentation/load-services'
 import { type ServiceModel, StatusService } from './../../../src/domain/model/service-model'
 
-class LoadServicesStub implements LoadServices {
-  async load (): Promise<ServiceModel[] | null> {
-    return await Promise.resolve([{
-      id: 'any_id',
-      name: 'any_name',
-      price: 30,
-      star: 5,
-      status: StatusService.active,
-      category: 'any_category'
-    }])
+interface SutTypes {
+  sut: LoadServicesController
+  loadServicesStub: LoadServices
+}
+
+const makeSut = (): SutTypes => {
+  const loadServicesStub = makeLoadServicesStub()
+  const sut = new LoadServicesController(loadServicesStub)
+
+  return {
+    sut,
+    loadServicesStub
   }
+}
+
+const makeLoadServicesStub = (): LoadServices => {
+  class LoadServicesStub implements LoadServices {
+    async load (): Promise<ServiceModel[] | null> {
+      return await Promise.resolve([{
+        id: 'any_id',
+        name: 'any_name',
+        price: 30,
+        star: 5,
+        status: StatusService.active,
+        category: 'any_category'
+      }])
+    }
+  }
+
+  return new LoadServicesStub()
 }
 
 describe('LoadServicesController', () => {
   test('should call LoadServices', async () => {
-    const loadServicesStub = new LoadServicesStub()
-    const sut = new LoadServicesController(loadServicesStub)
+    const { sut, loadServicesStub } = makeSut()
     const loadSpy = jest.spyOn(loadServicesStub, 'load')
     await sut.handle({})
     expect(loadSpy).toHaveBeenCalled()
   })
 
   test('should return 204 if LoadServices returns null', async () => {
-    const loadServicesStub = new LoadServicesStub()
-    const sut = new LoadServicesController(loadServicesStub)
+    const { sut, loadServicesStub } = makeSut()
     jest.spyOn(loadServicesStub, 'load').mockResolvedValue(null)
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual({
@@ -36,9 +53,8 @@ describe('LoadServicesController', () => {
   })
 
   test('should return 500 if LoadServices throws', async () => {
-    const loadServicesStub = new LoadServicesStub()
+    const { sut, loadServicesStub } = makeSut()
     const error = new Error()
-    const sut = new LoadServicesController(loadServicesStub)
     jest.spyOn(loadServicesStub, 'load').mockRejectedValueOnce(error)
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual({
