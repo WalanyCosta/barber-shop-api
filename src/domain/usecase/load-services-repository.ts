2170f8dbba0dd9f6b447666dfbaf8d6f6feffb9c
WalanyCosta <walanybnegro@gmail.com>
2024-03-07 03:02:40 +0100

    feat: ensure DbLoadServices calls LoadServicesRepository

---
+++ src/domain/usecase/load-services-repository.ts
@@ -0,0 +1,11 @@
+import { type ServiceModel } from '../model/service-model'
+import { type LoadServicesRepository } from '../protocols/infra/db/load-service-repository'
+import { type LoadServices } from '../protocols/presentation/load-services'
+
+export class DbLoadServices implements LoadServices {
+  constructor (private readonly loadServicesRepository: LoadServicesRepository) {}
+  async load (): Promise<ServiceModel[] | null> {
+    await this.loadServicesRepository.load()
+    return await Promise.resolve(null)
+  }
+}
