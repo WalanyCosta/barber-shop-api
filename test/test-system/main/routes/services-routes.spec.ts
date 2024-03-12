18c42c7c9de6ed43387eb7610518a3bdbd29b658
WalanyCosta <walanybnegro@gmail.com>
2024-03-12 01:07:57 +0100

    test: ensure route get services return 200 on success

--- test/test-system/main/routes/services-routes.spec.ts
+++ test/test-system/main/routes/services-routes.spec.ts
@@ -1,6 +1,7 @@
 import { cleanData, disconnect } from '../../../../src/infra/db/prisma/helpers/prisma-helper'
 import request from 'supertest'
 import app from '../../../../src/main/config/app'
+import prisma from '../../../../src/infra/db/prisma/helpers/client'
 
 describe('Get /services', () => {
   beforeEach(async () => {
@@ -16,5 +17,25 @@ describe('Get /services', () => {
     await request(app)
       .get('/api/services')
       .expect(204)
+  })
+
+  test('should return 200 on success', async () => {
+    await prisma.service.create({
+      data: {
+        name: 'any_name',
+        price: 30.3,
+        stars: 3,
+        status: 'active',
+        category: {
+          create: {
+            category: 'any_category'
+          }
+        }
+      }
+    })
+
+    await request(app)
+      .get('/api/services')
+      .expect(200)
   })
 })
