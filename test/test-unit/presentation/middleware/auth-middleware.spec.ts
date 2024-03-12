482a2819a2d998e2cb27181d78d7de7664b0da9b
WalanyCosta <walanybnegro@gmail.com>
2024-03-12 04:45:01 +0100

    refactor: move SUT to creation factory method

--- test/test-unit/presentation/middleware/auth-middleware.spec.ts
+++ test/test-unit/presentation/middleware/auth-middleware.spec.ts
@@ -2,6 +2,21 @@ import { AccessDeniedError } from './../../../../src/presentation/errors/access-
 import { AuthMiddleware } from './../../../../src/presentation/middleware/auth-middleware'
 import { type LoadAccountByToken } from './../../../../src/domain/protocols/presentation/load-account-by-token'
 
+interface SutTypes {
+  sut: AuthMiddleware
+  loadAccountByTokenStub: LoadAccountByToken
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByTokenStub = makeLoadAccountByToken()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
+
+  return {
+    sut,
+    loadAccountByTokenStub
+  }
+}
+
 const makeLoadAccountByToken = (): LoadAccountByToken => {
   class LoadAccountByTokenStub implements LoadAccountByToken {
     async load (accessToken: string): Promise<string> {
@@ -14,8 +29,7 @@ const makeLoadAccountByToken = (): LoadAccountByToken => {
 
 describe('Auth Middleware', () => {
   test('should call LoadAccountByToken with correct param', async () => {
-    const loadAccountByTokenStub = makeLoadAccountByToken()
-    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    const { sut, loadAccountByTokenStub } = makeSut()
     const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
     const httpRequest = {
       headers: {
@@ -27,8 +41,7 @@ describe('Auth Middleware', () => {
   })
 
   test('should return 403 if LoadAccountByToken returns null', async () => {
-    const loadAccountByTokenStub = makeLoadAccountByToken()
-    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    const { sut, loadAccountByTokenStub } = makeSut()
     jest.spyOn(loadAccountByTokenStub, 'load').mockResolvedValueOnce(null)
     const httpRequest = {
       headers: {
@@ -43,8 +56,7 @@ describe('Auth Middleware', () => {
   })
 
   test('should return 500 if LoadAccountByToken throws', async () => {
-    const loadAccountByTokenStub = makeLoadAccountByToken()
-    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    const { sut, loadAccountByTokenStub } = makeSut()
     const error = new Error()
     jest.spyOn(loadAccountByTokenStub, 'load').mockRejectedValueOnce(error)
     const httpRequest = {
@@ -60,8 +72,7 @@ describe('Auth Middleware', () => {
   })
 
   test('should return 403 if no x-access-token in headers', async () => {
-    const loadAccountByTokenStub = makeLoadAccountByToken()
-    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual({
       statusCode: 403,
