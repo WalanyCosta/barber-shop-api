3d8b3fe79cbbebda26a59793ff56b6d843bc9999
WalanyCosta <walanybnegro@gmail.com>
2024-03-14 02:07:41 +0100

    refactor: move SUT to creation factory method

--- test/test-unit/infra/jw-adapter/jw-adapter.spec.ts
+++ test/test-unit/infra/jw-adapter/jw-adapter.spec.ts
@@ -6,30 +6,32 @@ jest.mock('jsonwebtoken', () => ({
     return 'any_token'
   },
   verify (): string {
-    return ''
+    return 'any_value'
   }
 }))
+
+const makeSut = (privateKey = 'secret'): JwtAdapter => {
+  return new JwtAdapter(privateKey)
+}
 
 describe('JWT Adapter encrypter', () => {
   test('should call sign with correct param', async () => {
     const privatekey = 'secret'
-    const sut = new JwtAdapter(privatekey)
+    const sut = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, privatekey)
   })
 
   test('should throw if sign throws', async () => {
-    const privatekey = 'secret'
-    const sut = new JwtAdapter(privatekey)
+    const sut = makeSut()
     jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
     const error = sut.encrypt('any_id')
     await expect(error).rejects.toThrow()
   })
 
   test('should return accessToken on success', async () => {
-    const privatekey = 'secret'
-    const sut = new JwtAdapter(privatekey)
+    const sut = makeSut()
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
   })
@@ -38,7 +40,7 @@ describe('JWT Adapter encrypter', () => {
 describe('JWT Adapter decrypter', () => {
   test('should call verify with correct params', async () => {
     const privatekey = 'secret'
-    const sut = new JwtAdapter(privatekey)
+    const sut = makeSut()
     const signSpy = jest.spyOn(jwt, 'verify')
     await sut.decrypt('token')
     expect(signSpy).toHaveBeenCalledWith('token', privatekey)
