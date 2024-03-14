e59d65e8ce611973f00d6a6ba0db48c56e37e6a2
WalanyCosta <walanybnegro@gmail.com>
2024-03-15 00:06:00 +0100

    refactor: change all tests in route services adding token to no fails

--- test/test-system/main/routes/services-routes.spec.ts
+++ test/test-system/main/routes/services-routes.spec.ts
@@ -1,7 +1,33 @@
+import jwt from 'jsonwebtoken'
 import { cleanData, disconnect } from '../../../../src/infra/db/prisma/helpers/prisma-helper'
 import request from 'supertest'
 import app from '../../../../src/main/config/app'
 import prisma from '../../../../src/infra/db/prisma/helpers/client'
+
+const mockAccessToken = async (): Promise<string> => {
+  const account = await prisma.account.create({
+    data: {
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password',
+      phone: 'any_number_phone',
+      accessToken: ''
+    }
+  })
+
+  const token = jwt.sign({ id: account.id }, String(process.env.PRIVATE_KEY))
+
+  await prisma.account.update({
+    where: {
+      id: account.id
+    },
+    data: {
+      accessToken: token
+    }
+  })
+
+  return token
+}
 
 describe('Get /services', () => {
   beforeEach(async () => {
@@ -14,12 +40,16 @@ describe('Get /services', () => {
   })
 
   test('should return 204 when database is empty', async () => {
+    const accessToken = await mockAccessToken()
     await request(app)
       .get('/api/services')
+      .set('x-access-token', accessToken)
       .expect(204)
   })
 
   test('should return 200 on success', async () => {
+    const accessToken = await mockAccessToken()
+
     await prisma.service.create({
       data: {
         name: 'any_name',
@@ -36,6 +66,7 @@ describe('Get /services', () => {
 
     await request(app)
       .get('/api/services')
+      .set('x-access-token', accessToken)
       .expect(200)
   })
 })
