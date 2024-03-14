96ca3d9544d768cdf6b21bcbfa8cb2ac2c4b194d
WalanyCosta <walanybnegro@gmail.com>
2024-03-14 01:58:57 +0100

    test: ensure JwtAdapter calls verify with correct params

--- test/test-unit/infra/jw-adapter/jw-adapter.spec.ts
+++ test/test-unit/infra/jw-adapter/jw-adapter.spec.ts
@@ -4,10 +4,13 @@ import jwt from 'jsonwebtoken'
 jest.mock('jsonwebtoken', () => ({
   sign (): string {
     return 'any_token'
+  },
+  verify (): string {
+    return ''
   }
 }))
 
-describe('JWT Adapter', () => {
+describe('JWT Adapter encrypter', () => {
   test('should call sign with correct param', async () => {
     const privatekey = 'secret'
     const sut = new JwtAdapter(privatekey)
@@ -29,5 +32,15 @@ describe('JWT Adapter', () => {
     const sut = new JwtAdapter(privatekey)
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
+  })
+})
+
+describe('JWT Adapter decrypter', () => {
+  test('should call verify with correct params', async () => {
+    const privatekey = 'secret'
+    const sut = new JwtAdapter(privatekey)
+    const signSpy = jest.spyOn(jwt, 'verify')
+    await sut.decrypt('token')
+    expect(signSpy).toHaveBeenCalledWith('token', privatekey)
   })
 })
