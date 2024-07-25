d0a488b172b57d6dce57e0d02d790b05a1d90601
WalanyCosta <walanybnegro@gmail.com>
2024-07-25 17:05:51 +0100

    feat: create loadServiceByIdService

---
+++ src/main/factories/services/load-service-by-id-factory.ts
@@ -0,0 +1,12 @@
+import { type Controller } from '@/presentation/protocols/controller'
+import { ServicesRepository } from '@/infra/db/prisma'
+import { LoadServiceByIdController } from '@/presentation/controller'
+import { DbLoadServiceById } from '@/domain/usecase/db-load-service-by-id'
+
+export const makeLoadServiceByIdController = (): Controller => {
+  const servicesRepository = new ServicesRepository()
+  const dbLoadServiceById = new DbLoadServiceById(servicesRepository)
+  const loadServiceByIdController = new LoadServiceByIdController(dbLoadServiceById)
+
+  return loadServiceByIdController
+}
