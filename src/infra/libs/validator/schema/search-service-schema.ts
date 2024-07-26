b78719415eff8dde64563004228228717ed57c7f
WalanyCosta <walanybnegro@gmail.com>
2024-07-26 12:19:05 +0100

    feat: ensure ZodValidation validates SearchServicesController

---
+++ src/infra/libs/validator/schema/search-service-schema.ts
@@ -0,0 +1,7 @@
+import * as z from 'zod'
+
+export const searchServiceSchema = z.object({
+  typeQuery: z.enum(['service', 'category'], { required_error: 'typeQuery is required.' }),
+  query: z.string({ required_error: 'query is required.' })
+    .min(1, { message: 'query is empty. Please to write query.' })
+})
