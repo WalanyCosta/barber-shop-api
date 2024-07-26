2ef43ab9641e1e1631bbd351ff2a308099bebbb4
WalanyCosta <walanybnegro@gmail.com>
2024-07-26 12:32:25 +0100

    test: ensure SearchServicesController returns 200 if filter returns empty array

--- test/presentation/controller/services/search-services-controller.spec.ts
+++ test/presentation/controller/services/search-services-controller.spec.ts
@@ -14,4 +14,20 @@ describe('SearchServicesController', () => {
     })
     expect(filterSpy).toHaveBeenCalledWith('service', 'any_query')
   })
+
+  test('should return 200 if filter returns empty array', async () => {
+    const searchServiceStub = makeSearchServicesStub()
+    const sut = new SearchServicesController(searchServiceStub)
+    jest.spyOn(searchServiceStub, 'filter').mockReturnValueOnce(Promise.resolve([]))
+    const response = await sut.handle({
+      query: {
+        typeQuery: 'service',
+        query: 'any_query'
+      }
+    })
+    expect(response).toEqual({
+      statusCode: 200,
+      body: []
+    })
+  })
 })
