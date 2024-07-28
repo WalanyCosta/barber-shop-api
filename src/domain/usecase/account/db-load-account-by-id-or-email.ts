f74be1a3721e897bca3311bce1da9d18376f0ece
WalanyCosta <walanybnegro@gmail.com>
2024-07-28 17:25:19 +0100

    feat: ensure DbLoadAccountByIdOrEmail if LoadAccountByIdOrEmailRepository returns null

--- src/domain/usecase/account/db-load-account-by-id-or-email.ts
+++ src/domain/usecase/account/db-load-account-by-id-or-email.ts
@@ -1,11 +1,16 @@
 import { type AccountModel } from '@/domain/model/account-model'
 import { type LoadAccountByIdOrEmailRepository } from '@/domain/protocols/infra'
 import { type LoadAccountByIdOrEmail } from '@/domain/protocols/presentation/load-account-by-id'
+import { NotExistsRegister } from '@/presentation/errors'
 
 export class DbLoadAccountByIdOrEmail implements LoadAccountByIdOrEmail {
   constructor (private readonly loadAccountByIdOrEmailRepository: LoadAccountByIdOrEmailRepository) {}
   async loadByIdOrEmail (idOrEmail: string): Promise<AccountModel | null> {
-    await this.loadAccountByIdOrEmailRepository.load(idOrEmail)
-    return await Promise.resolve(null)
+    const account = await this.loadAccountByIdOrEmailRepository.load(idOrEmail)
+
+    if (!account) {
+      throw new NotExistsRegister('This services not exists')
+    }
+    return null
   }
 }
