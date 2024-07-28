901dff786689510b5bbfc287af6bdc968b834a2d
WalanyCosta <walanybnegro@gmail.com>
2024-07-28 11:27:43 +0100

    refactor: move SUT to creation factory method

--- test/domain/usecase/service/db-search-services.spec.ts
+++ test/domain/usecase/service/db-search-services.spec.ts
@@ -1,24 +1,36 @@
+import { type SearchServicesRepository } from '@/domain/protocols/infra/db/services/search-services-repository'
 import { makeSearchServicesRepositoryStub } from '../../mock/mock-service'
 import { DbSearchServices } from '@/domain/usecase/service/db-search-services'
 
+interface SutTypes {
+  sut: DbSearchServices
+  searchServicesRepositoryStub: SearchServicesRepository
+}
+
+const makeSut = (): SutTypes => {
+  const searchServicesRepositoryStub = makeSearchServicesRepositoryStub()
+  const sut = new DbSearchServices(searchServicesRepositoryStub)
+
+  return {
+    sut,
+    searchServicesRepositoryStub
+  }
+}
+
 describe('DbSearchServices', () => {
   test('should call filter with correct params', async () => {
-    const searchServicesRepositoryStub = makeSearchServicesRepositoryStub()
-    const sut = new DbSearchServices(searchServicesRepositoryStub)
     const typeQuery = 'service'
     const query = 'any_service'
+    const { sut, searchServicesRepositoryStub } = makeSut()
     const filterSpy = jest.spyOn(searchServicesRepositoryStub, 'filter')
     await sut.filter(typeQuery, query)
     expect(filterSpy).toHaveBeenCalledWith(typeQuery, query)
   })
 
   test('should throws if filter throws', async () => {
-    const searchServicesRepositoryStub = makeSearchServicesRepositoryStub()
-    const sut = new DbSearchServices(searchServicesRepositoryStub)
-    const typeQuery = 'service'
-    const query = 'any_service'
+    const { sut, searchServicesRepositoryStub } = makeSut()
     jest.spyOn(searchServicesRepositoryStub, 'filter').mockRejectedValueOnce(new Error())
-    const error = sut.filter(typeQuery, query)
+    const error = sut.filter('service', 'any_service')
     expect(error).rejects.toThrow()
   })
 })
