248f8fa9759c4c42ec2c7749124ffb20470e4ea3
WalanyCosta <walanybnegro@gmail.com>
2024-07-28 16:19:04 +0100

    feat: ensure LoadAccountByIdController returns 400 if loadByIdOrEmail throws NotExistsRegister

--- src/domain/protocols/presentation/load-account-by-id.ts
+++ src/domain/protocols/presentation/load-account-by-id.ts
@@ -1,5 +1,5 @@
 import { type AccountModel } from '@/domain/model/account-model'
 
 export interface LoadAccountByIdOrEmail {
-  loadByIdOrEmail: (idOrEmail: string) => Promise<AccountModel>
+  loadByIdOrEmail: (idOrEmail: string) => Promise<AccountModel | null>
 }
--- src/presentation/controller/account/load-account-by-id-controller.ts
+++ src/presentation/controller/account/load-account-by-id-controller.ts
@@ -1,4 +1,5 @@
 import { type LoadAccountByIdOrEmail } from '@/domain/protocols/presentation/load-account-by-id'
+import { NotExistsRegister } from '@/presentation/errors'
 import { type Controller } from '@/presentation/protocols/controller'
 import { type HttpRequest, type HttpResponse } from '@/presentation/protocols/http'
 
@@ -9,11 +10,18 @@ export class LoadAccountByIdController implements Controller {
     try {
       const { id } = httpRequest.params
       await this.loadAccountByIdOrEmail.loadByIdOrEmail(id)
+
       return await Promise.resolve({
         statusCode: 400,
         body: null
       })
     } catch (error) {
+      if (error instanceof NotExistsRegister) {
+        return {
+          statusCode: 400,
+          body: error
+        }
+      }
       return {
         statusCode: 500,
         body: error
