f17213d7e8b80a3d7b83cd5c6a4ca40c6697d802
WalanyCosta <walanybnegro@gmail.com>
2024-07-28 17:26:10 +0100

    test: ensure DbLoadAccountByIdOrEmail if LoadAccountByIdOrEmailRepository returns null

--- test/domain/usecase/account/db-load-account-by-id-or-email.spec.ts
+++ test/domain/usecase/account/db-load-account-by-id-or-email.spec.ts
@@ -1,12 +1,21 @@
-import { makeLoadAccountByIdOrEmailRepositoryStub } from '../../mock/mock-account'
+import { NotExistsRegister } from '@/presentation/errors'
+import { makeLoadAccountByIdOrEmailRepositoryStub, mockAccountModel } from '../../mock/mock-account'
 import { DbLoadAccountByIdOrEmail } from '@/domain/usecase/account/db-load-account-by-id-or-email'
 
 describe('DbLoadAccountByIdOrEmail', () => {
   test('should call LoadAccountByIdOrEmailRepository with correct id or email', async () => {
-    const loadAccountByIdOrEmailRepositoryStub = makeLoadAccountByIdOrEmailRepositoryStub()
+    const loadAccountByIdOrEmailRepositoryStub = makeLoadAccountByIdOrEmailRepositoryStub(mockAccountModel)
     const sut = new DbLoadAccountByIdOrEmail(loadAccountByIdOrEmailRepositoryStub)
     const loadByIdOrEmailSpy = jest.spyOn(loadAccountByIdOrEmailRepositoryStub, 'load')
     await sut.loadByIdOrEmail('any_id')
     expect(loadByIdOrEmailSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  test('should throw if LoadAccountByIdOrEmailRepository returns null', async () => {
+    const loadAccountByIdOrEmailRepositoryStub = makeLoadAccountByIdOrEmailRepositoryStub(mockAccountModel)
+    const sut = new DbLoadAccountByIdOrEmail(loadAccountByIdOrEmailRepositoryStub)
+    jest.spyOn(loadAccountByIdOrEmailRepositoryStub, 'load').mockResolvedValueOnce(null)
+    const response = sut.loadByIdOrEmail('any_id')
+    await expect(response).rejects.toThrow(new NotExistsRegister('This services not exists'))
   })
 })
