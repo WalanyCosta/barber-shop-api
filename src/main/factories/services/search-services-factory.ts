ffe26ab7cdf0b47902cd888fa5686f58bfc87d99
WalanyCosta <walanybnegro@gmail.com>
2024-07-28 12:04:51 +0100

    feat: create SearchServices Factory

---
+++ src/main/factories/services/search-services-factory.ts
@@ -0,0 +1,15 @@
+import { searchServiceSchema } from '@/infra/libs/validator/schema/search-service-schema'
+import { type Controller } from '@/presentation/protocols/controller'
+import { ServicesRepository } from '@/infra/db/prisma'
+import { DbSearchServices } from '@/domain/usecase/service/db-search-services'
+import { SearchServicesController } from '@/presentation/controller'
+import { ZodValidator } from '@/infra/libs'
+
+export const makeSearchServicesFactory = (): Controller => {
+  const servicesRepository = new ServicesRepository()
+  const dbSearchServices = new DbSearchServices(servicesRepository)
+  const zodValidator = new ZodValidator(searchServiceSchema)
+  const loadServicesController = new SearchServicesController(dbSearchServices, zodValidator)
+
+  return loadServicesController
+}
