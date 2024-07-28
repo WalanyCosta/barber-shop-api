273ce679d4ccf13f1b6c248f637a79b5d1a0cda7
WalanyCosta <walanybnegro@gmail.com>
2024-07-28 09:35:26 +0100

    test: ensure SearchServiceController returns 400 if validate throws

--- test/presentation/controller/services/search-services-controller.spec.ts
+++ test/presentation/controller/services/search-services-controller.spec.ts
@@ -1,3 +1,4 @@
+import { ValidationError } from '@/presentation/errors'
 import { makeSearchServicesStub, makeValidatorStub } from '../../mocks'
 import { SearchServicesController } from '@/presentation/controller'
 
@@ -65,6 +66,24 @@ describe('SearchServicesController', () => {
     expect(validateSpy).toHaveBeenCalledWith({
       typeQuery: 'service',
       query: 'any_query'
+    })
+  })
+
+  test('should return 400 if validate throws', async () => {
+    const searchServiceStub = makeSearchServicesStub()
+    const validatorStub = makeValidatorStub()
+    const error = new ValidationError('Ocorreu um error')
+    const sut = new SearchServicesController(searchServiceStub, validatorStub)
+    jest.spyOn(validatorStub, 'validate').mockReturnValueOnce(error)
+    const response = await sut.handle({
+      query: {
+        typeQuery: 'service',
+        query: ''
+      }
+    })
+    expect(response).toEqual({
+      statusCode: 400,
+      body: error
     })
   })
 })
