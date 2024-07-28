e18eda6bffdf594acb8d31e54e7a02cd98696e0e
WalanyCosta <walanybnegro@gmail.com>
2024-07-28 11:20:20 +0100

    test: ensure DbSearchServices throws if filter throws error

--- test/domain/usecase/service/db-search-services.spec.ts
+++ test/domain/usecase/service/db-search-services.spec.ts
@@ -11,4 +11,14 @@ describe('DbSearchServices', () => {
     await sut.filter(typeQuery, query)
     expect(filterSpy).toHaveBeenCalledWith(typeQuery, query)
   })
+
+  test('should throws if filter throws', async () => {
+    const searchServicesRepositoryStub = makeSearchServicesRepositoryStub()
+    const sut = new DbSearchServices(searchServicesRepositoryStub)
+    const typeQuery = 'service'
+    const query = 'any_service'
+    jest.spyOn(searchServicesRepositoryStub, 'filter').mockRejectedValueOnce(new Error())
+    const error = sut.filter(typeQuery, query)
+    expect(error).rejects.toThrow()
+  })
 })
