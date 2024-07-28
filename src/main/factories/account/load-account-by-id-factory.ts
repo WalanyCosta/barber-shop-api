77d900cdeacf0151e72b063d18ddaa7b98647f1e
WalanyCosta <walanybnegro@gmail.com>
2024-07-28 17:57:05 +0100

    feat: create LoadAccountByIdFactory

---
+++ src/main/factories/account/load-account-by-id-factory.ts
@@ -0,0 +1,11 @@
+import { type Controller } from '@/presentation/protocols/controller'
+import { AccountRepository } from '@/infra/db/prisma'
+import { LoadAccountByIdController } from '@/presentation/controller'
+import { DbLoadAccountByIdOrEmail } from '@/domain/usecase/account/db-load-account-by-id-or-email'
+
+export const makeLoadAccountByIdFactory = (): Controller => {
+  const makeAccountRepository = new AccountRepository()
+  const dbLoadAccountByIdOrEmail = new DbLoadAccountByIdOrEmail(makeAccountRepository)
+  const loadAccountByIdController = new LoadAccountByIdController(dbLoadAccountByIdOrEmail)
+  return loadAccountByIdController
+}
