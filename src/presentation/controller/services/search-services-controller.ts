b69a0e284499a4acd2fa3948b6c4442c0e14b06f
WalanyCosta <walanybnegro@gmail.com>
2024-07-28 09:21:53 +0100

    feat: add SearchServicesController calls validate with correct params

--- src/presentation/controller/services/search-services-controller.ts
+++ src/presentation/controller/services/search-services-controller.ts
@@ -1,14 +1,15 @@
 import { type SearchServices } from '@/domain/protocols/presentation/search-service'
 import { type Controller } from '@/presentation/protocols/controller'
 import { type HttpRequest, type HttpResponse } from '@/presentation/protocols/http'
+import { type Validator } from '@/presentation/protocols/validator'
 
 export class SearchServicesController implements Controller {
-  constructor (private readonly searchServices: SearchServices) {}
+  constructor (private readonly searchServices: SearchServices, private readonly validator: Validator) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const { typeQuery, query } = httpRequest?.query
-      const services = await this.searchServices.filter(typeQuery, query)
+      this.validator.validate(httpRequest.query)
+      const services = await this.searchServices.filter(httpRequest.query?.typeQuery, httpRequest.query?.query)
       return await Promise.resolve({
         statusCode: 200,
         body: services
