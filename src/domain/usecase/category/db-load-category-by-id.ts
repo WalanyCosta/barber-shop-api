1e7b4a0d42745587e7cceb31f1c361b388c23773
WalanyCosta <walanybnegro@gmail.com>
2024-07-29 17:25:34 +0100

    feat: ensure DbLoadCategoryById throws if LoadCategoryByIdRepository throws NotExistsRegister

--- src/domain/usecase/category/db-load-category-by-id.ts
+++ src/domain/usecase/category/db-load-category-by-id.ts
@@ -1,11 +1,15 @@
 import { type CategoryModel } from '@/domain/model/category-model'
 import { type LoadCategoryByIdRepository } from '@/domain/protocols/infra/db/category/load-category-by-id-repository'
 import { type LoadCategoryById } from '@/domain/protocols/presentation'
+import { NotExistsRegister } from '@/presentation/errors'
 
 export class DbLoadCategoryById implements LoadCategoryById {
   constructor (private readonly loadCategoryByIdRepository: LoadCategoryByIdRepository) {}
   async loadById (id: string): Promise<CategoryModel | null> {
-    await this.loadCategoryByIdRepository.loadById(id)
+    const category = await this.loadCategoryByIdRepository.loadById(id)
+    if (!category) {
+      throw new NotExistsRegister('Not exists register with this id')
+    }
     return null
   }
 }
