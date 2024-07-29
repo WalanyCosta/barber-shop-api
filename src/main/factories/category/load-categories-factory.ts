b3cc9e7eb7d11ee0b79e3861251adde67b16d63a
WalanyCosta <walanybnegro@gmail.com>
2024-07-29 14:48:41 +0100

    feat: add LoadCategories factory

---
+++ src/main/factories/category/load-categories-factory.ts
@@ -0,0 +1,12 @@
+import { type Controller } from '@/presentation/protocols/controller'
+import { DbLoadCategories } from '@/domain/usecase'
+import { CategoryRepository } from '@/infra/db/prisma'
+import { LoadCategoriesController } from '@/presentation/controller'
+
+export const makeLoadCategoriesFactory = (): Controller => {
+  const categoryRepository = new CategoryRepository()
+  const dbLoadCategories = new DbLoadCategories(categoryRepository)
+  const loadCategoriesController = new LoadCategoriesController(dbLoadCategories)
+
+  return loadCategoriesController
+}
