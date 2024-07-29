9661019990fa77ab9529a131248b6dc67565d41b
WalanyCosta <walanybnegro@gmail.com>
2024-07-29 17:58:48 +0100

    feat: add route /categories/:id

--- src/main/routes/category-routes.ts
+++ src/main/routes/category-routes.ts
@@ -2,7 +2,9 @@ import { type Router } from 'express'
 import { auth } from '../config/auth'
 import adapterRoute from '@/main/adapter/express/express-route-adapter'
 import { makeLoadCategoriesFactory } from '../factories/category/load-categories-factory'
+import { makeLoadCategoryByIdRepositoryFactory } from '../factories/category/load-category-by-id-factory'
 
 export default (router: Router): void => {
   router.get('/categories', auth, adapterRoute(makeLoadCategoriesFactory()))
+  router.get('/categories/:id', auth, adapterRoute(makeLoadCategoryByIdRepositoryFactory()))
 }
