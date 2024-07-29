35c492542633f39f57d2e55c91f0ec6e183ad9f3
WalanyCosta <walanybnegro@gmail.com>
2024-07-29 17:47:29 +0100

    feat: ensure CategoryRepository returns category on success

--- src/infra/db/prisma/repositories/category/category-repository.ts
+++ src/infra/db/prisma/repositories/category/category-repository.ts
@@ -1,10 +1,19 @@
 import { CategoryModel } from "@/domain/model/category-model";
-import { LoadCategoriesRepository } from "@/domain/protocols/infra/db";
+import { LoadCategoriesRepository, LoadCategoryByIdRepository } from "@/domain/protocols/infra/db";
 import prisma from "../../helpers/client";
 
-export class CategoryRepository implements LoadCategoriesRepository{
+export class CategoryRepository implements LoadCategoriesRepository, LoadCategoryByIdRepository{
     async load(): Promise<CategoryModel[]>{
         const categories = await prisma.category.findMany()
         return categories
+    }
+
+    async loadById(id: string): Promise<CategoryModel | null>{
+        const category = await prisma.category.findUnique({
+            where:{
+                id
+            }
+        })
+        return category
     }
 }
