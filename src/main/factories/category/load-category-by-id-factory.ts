5dc8542b9cbfe52af3a63dbf84152867340619e1
WalanyCosta <walanybnegro@gmail.com>
2024-07-29 17:53:00 +0100

    feat: add LoadCategoryById factory

---
+++ src/main/factories/category/load-category-by-id-factory.ts
@@ -0,0 +1,12 @@
+import { type Controller } from '@/presentation/protocols/controller'
+import { DbLoadCategoryById } from '@/domain/usecase'
+import { CategoryRepository } from '@/infra/db/prisma'
+import { LoadCategoryByIdController } from '@/presentation/controller'
+
+export const makeLoadCategoryByIdRepositoryFactory = (): Controller => {
+  const categoryRepository = new CategoryRepository()
+  const dbLoadCategoryById = new DbLoadCategoryById(categoryRepository)
+  const loadCategoryByIdController = new LoadCategoryByIdController(dbLoadCategoryById)
+
+  return loadCategoryByIdController
+}
