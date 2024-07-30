aaff2071379774fb0f0479b1297e0abe891bbad7
WalanyCosta <walanybnegro@gmail.com>
2024-07-30 16:06:57 +0100

    feat: ensure DbLoadBarberById throws if LoadBarberByIdRepository returns null

--- src/domain/usecase/barber/db-load-barber-by-id.ts
+++ src/domain/usecase/barber/db-load-barber-by-id.ts
@@ -1,11 +1,15 @@
 import { type BarberModel } from '@/domain/model/barber-model'
 import { type LoadBarberByIdRepository } from '@/domain/protocols/infra/db'
 import { type LoadBarberById } from '@/domain/protocols/presentation'
+import { NotExistsRegister } from '@/presentation/errors'
 
 export class DbLoadBarberById implements LoadBarberById {
   constructor (private readonly loadBarberByIdRepository: LoadBarberByIdRepository) {}
   async loadById (id: string): Promise<BarberModel | null> {
-    await this.loadBarberByIdRepository.loadById(id)
+    const barber = await this.loadBarberByIdRepository.loadById(id)
+    if (!barber) {
+      throw new NotExistsRegister('No exists register with id')
+    }
     return null
   }
 }
