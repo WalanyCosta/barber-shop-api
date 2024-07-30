ac87651f480a29eeaed0e969f160234791a5fbd5
WalanyCosta <walanybnegro@gmail.com>
2024-07-30 17:22:44 +0100

    test: ensure route /barbers/:id returns statuscode 200 on success

---
+++ test/main/routes/barber-routes.spec.ts
@@ -0,0 +1,67 @@
+import jwt from 'jsonwebtoken'
+import { cleanData, disconnect } from '@/infra/db/prisma/helpers/prisma-helper'
+import request from 'supertest'
+import app from '@/main/config/app'
+import prisma from '@/infra/db/prisma/helpers/client'
+
+const mockAccessToken = async (): Promise<string> => {
+  const account = await prisma.account.create({
+    data: {
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password',
+      phone: 'any_number_phone',
+      accessToken: ''
+    }
+  })
+
+  const token = jwt.sign({ id: account.id }, String(process.env.PRIVATE_KEY))
+
+  await prisma.account.update({
+    where: {
+      id: account.id
+    },
+    data: {
+      accessToken: token
+    }
+  })
+
+  return token
+}
+
+const mockCreateBarber = async (): Promise<any> => {
+  return await prisma.barber.create({
+    data: {
+      id: 'any_id',
+      name: 'any_id',
+      birthday: 'any_birthday',
+      email: 'any_email',
+      phone: 'any_phone',
+      experience: 'any_experience',
+      experience_year: 3,
+      start: 5,
+      status: 'any_status',
+      image_url: 'any_image_url'
+    }
+  })
+}
+
+describe('Get /barbers/:id', () => {
+  beforeEach(async () => {
+    await cleanData()
+  })
+
+  afterAll(async () => {
+    await cleanData()
+    await disconnect()
+  })
+
+  test('should return 200 on success', async () => {
+    const accessToken = await mockAccessToken()
+    const { id } = await mockCreateBarber()
+    await request(app)
+      .get(`/api/barbers/${id}`)
+      .set('x-access-token', accessToken)
+      .expect(200)
+  })
+})
