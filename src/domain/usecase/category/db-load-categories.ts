5a81cebe0e65cdd042391c29ceb6a4031c017212
WalanyCosta <walanybnegro@gmail.com>
2024-07-31 12:07:59 +0100

    chore: add prettier

--- src/domain/usecase/category/db-load-categories.ts
+++ src/domain/usecase/category/db-load-categories.ts
@@ -1,11 +1,14 @@
-import { type CategoryModel } from '@/domain/model/category-model'
-import { type LoadCategoriesRepository } from '@/domain/protocols/infra/db'
-import { type LoadCategories } from '@/domain/protocols/presentation'
+import { type CategoryModel } from '@/domain/model/category-model';
+import { type LoadCategoriesRepository } from '@/domain/protocols/infra/db';
+import { type LoadCategories } from '@/domain/protocols/presentation';
 
 export class DbLoadCategories implements LoadCategories {
-  constructor (private readonly loadCategoriesRepository: LoadCategoriesRepository) {}
-  async load (): Promise<CategoryModel[]> {
-    const categories = await this.loadCategoriesRepository.load()
-    return categories
-  }
+    constructor(
+        private readonly loadCategoriesRepository: LoadCategoriesRepository,
+    ) {}
+
+    async load(): Promise<CategoryModel[]> {
+        const categories = await this.loadCategoriesRepository.load();
+        return categories;
+    }
 }
