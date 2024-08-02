e745b7a7aeddfa75a68a8fc60ccdc9bf1b541d1c
WalanyCosta <walanybnegro@gmail.com>
2024-08-02 16:23:03 +0100

    test: ensure BarberRepository returns empty array if status is disable

--- test/infra/db/barber-repository.spec.ts
+++ test/infra/db/barber-repository.spec.ts
@@ -2,10 +2,19 @@ import { cleanData } from '@/infra/db/prisma/helpers/prisma-helper'
 import prisma from '@/infra/db/prisma/helpers/client'
 import { BarberRepository } from '@/infra/db/prisma'
 import { mockBarberModel } from '../../helpers/mock-barber-model'
+import { StatusBarber } from '@/domain/model/barber-model'
 
 const createFakeBarberData = async (): Promise<any> => {
   return await prisma.barber.create({
     data: mockBarberModel,
+  })
+}
+
+const createFakeBarbersData = async (
+  status: StatusBarber = StatusBarber.active,
+): Promise<any> => {
+  return await prisma.barber.create({
+    data: { ...mockBarberModel, status },
   })
 }
 
@@ -26,5 +35,14 @@ describe('BarberRepository', () => {
     expect(barber?.birthday).toBe('any_birthday')
     expect(barber?.start).toBe(5)
     expect(barber?.email).toBe('any_email')
+  })
+
+  test('should return empty array if status is disable', async () => {
+    await createFakeBarbersData(StatusBarber.disable)
+    const sut = new BarberRepository()
+    const barbers = await sut.load(StatusBarber.active)
+
+    expect(barbers.length).toBe(0)
+    expect(barbers).toEqual([])
   })
 })
