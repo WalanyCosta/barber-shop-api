48984bb090251e3a2306a3e095eb7d470626becf
WalanyCosta <walanybnegro@gmail.com>
2024-08-02 16:36:12 +0100

    feat: add route /barbers

--- src/main/routes/barber-routes.ts
+++ src/main/routes/barber-routes.ts
@@ -2,7 +2,9 @@ import { type Router } from 'express'
 import { auth } from '../config/auth'
 import adapterRoute from '@/main/adapter/express/express-route-adapter'
 import { makeLoadBarberByIdFactory } from '../factories/barber/load-barber-by-id.-factory'
+import { makeLoadBarbersFactory } from '../factories/barber/load-barbers-factory'
 
 export default (router: Router): void => {
   router.get('/barbers/:id', auth, adapterRoute(makeLoadBarberByIdFactory()))
+  router.get('/barbers', auth, adapterRoute(makeLoadBarbersFactory()))
 }
