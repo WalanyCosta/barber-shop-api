b09781a3fa973d62047ab0187867965325c721ed
WalanyCosta <walanybnegro@gmail.com>
2024-08-05 17:05:29 +0100

    feat: ensure DbLoadTimeSchedules calls LoadBarberByIdRepository with correct id

--- src/domain/usecase/time-schedule/db-load-time-schedules.ts
+++ src/domain/usecase/time-schedule/db-load-time-schedules.ts
@@ -1,12 +1,15 @@
 import { StatusSchedule } from '@/domain/model/schedule-model'
 import { type LoadSchedulesByBarberIdRepository } from '@/domain/protocols/infra/db'
+import { type LoadBarberById } from '@/domain/protocols/presentation'
 
 export class DbLoadTimeSchedules {
   constructor(
     private readonly loadSchedulesByBarberIdRepository: LoadSchedulesByBarberIdRepository,
+    private readonly loadBarberById: LoadBarberById,
   ) {}
 
   async loadByBarberIDAndDate(barberId: string): Promise<any> {
+    await this.loadBarberById.loadById(barberId)
     await this.loadSchedulesByBarberIdRepository.loadByBarberId(
       barberId,
       StatusSchedule.WAITING,
