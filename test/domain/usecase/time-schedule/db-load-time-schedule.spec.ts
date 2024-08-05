60cb709f03d10b38aff2cb4b628c46560b189977
WalanyCosta <walanybnegro@gmail.com>
2024-08-05 15:35:01 +0100

    test: ensure DbLoadTimeSchedules calls LoadSchedulesByBarberIDRepository with params correct

---
+++ test/domain/usecase/time-schedule/db-load-time-schedule.spec.ts
@@ -0,0 +1,47 @@
+import {
+  type ScheduleModel,
+  StatusSchedule,
+} from '@/domain/model/schedule-model'
+import { type LoadSchedulesByBarberIdRepository } from '@/domain/protocols/infra/db'
+import { DbLoadTimeSchedules } from '@/domain/usecase'
+
+class LoadSchedulesByBarberIdRepositoryStub
+  implements LoadSchedulesByBarberIdRepository
+{
+  async loadByBarberId(
+    barberId: string,
+    statusSchedule: StatusSchedule,
+  ): Promise<ScheduleModel[]> {
+    return await Promise.resolve([
+      {
+        id: 'any_id',
+        status: 'WAITING',
+        total_time: 0,
+        barberId: 'any_barberId',
+      },
+      {
+        id: 'any_id',
+        status: 'WAITING',
+        total_time: 0,
+        barberId: 'any_barberId',
+      },
+    ])
+  }
+}
+
+describe('DbLoadTimeSchedules', () => {
+  test('should call LoadSchedulesByBarberIDRepository with params correct', async () => {
+    const loadSchedulesByBarberIDRepositoryStub =
+      new LoadSchedulesByBarberIdRepositoryStub()
+    const sut = new DbLoadTimeSchedules(loadSchedulesByBarberIDRepositoryStub)
+    const loadByBarberIdStub = jest.spyOn(
+      loadSchedulesByBarberIDRepositoryStub,
+      'loadByBarberId',
+    )
+    await sut.loadByBarberIDAndDate('any_barberId')
+    expect(loadByBarberIdStub).toHaveBeenCalledWith(
+      'any_barberId',
+      StatusSchedule.WAITING,
+    )
+  })
+})
