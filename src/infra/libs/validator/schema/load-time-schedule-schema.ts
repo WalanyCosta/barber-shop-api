f6cb97cd40e1f9ff263aba4a5a27be03a2561882
WalanyCosta <walanybnegro@gmail.com>
2024-08-09 16:07:15 +0100

    feat: ensure LoadTimeScheduleSchema returns error if barberId type if invalid

--- src/infra/libs/validator/schema/load-time-schedule-schema.ts
+++ src/infra/libs/validator/schema/load-time-schedule-schema.ts
@@ -1,5 +1,8 @@
 import * as z from 'zod'
 
 export const loadTimeScheduleSchema = z.object({
-  barberId: z.string({ required_error: 'barberId is required' }),
+  barberId: z.string({
+    required_error: 'barberId is required',
+    invalid_type_error: 'barberId type is invalid',
+  }),
 })
