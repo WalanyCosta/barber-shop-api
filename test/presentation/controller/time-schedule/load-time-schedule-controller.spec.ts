f37d622db4e1258006eb210a1d1f77e6b31cf923
WalanyCosta <walanybnegro@gmail.com>
2024-08-09 13:49:23 +0100

    test: ensure LoadTimeScheduleController throws if LoadTimeSchedule throws DateInvalidError

--- test/presentation/controller/time-schedule/load-time-schedule-controller.spec.ts
+++ test/presentation/controller/time-schedule/load-time-schedule-controller.spec.ts
@@ -1,3 +1,4 @@
+import { DateInvalidError } from '@/domain/errors/date-invalid-error'
 import { makeLoadTimeScheduleStub } from '../../mocks/mock-time-schedule'
 import { LoadTimeScheduleController } from '@/presentation/controller'
 
@@ -20,5 +21,26 @@ describe('LoadTimeScheduleController', () => {
       },
     })
     expect(loadByBarberIDAndDateSpy).toHaveBeenCalledWith(barberId, date)
+  })
+
+  test('should throw if LoadTimeSchedule throws DateInvalidError', async () => {
+    const error = new DateInvalidError('Essa data já passou!')
+    const loadTimeScheduleStub = makeLoadTimeScheduleStub()
+    const sut = new LoadTimeScheduleController(loadTimeScheduleStub)
+    jest
+      .spyOn(loadTimeScheduleStub, 'loadByBarberIDAndDate')
+      .mockRejectedValueOnce(error)
+    const response = await sut.handle({
+      params: {
+        barberId: 'any_barberId',
+      },
+      query: {
+        dateSchedule: 'yesterday_date',
+      },
+    })
+    expect(response).toEqual({
+      statusCode: 400,
+      body: error,
+    })
   })
 })
