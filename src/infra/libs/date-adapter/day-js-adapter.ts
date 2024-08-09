79d22c5f3f88b844d14ff912c196bf23669509ea
WalanyCosta <walanybnegro@gmail.com>
2024-08-09 12:31:04 +0100

    feat: ensure DayjsAdapter returns true on success

--- src/infra/libs/date-adapter/day-js-adapter.ts
+++ src/infra/libs/date-adapter/day-js-adapter.ts
@@ -4,10 +4,12 @@ import { type VerifyDateIsCurrent } from '@/domain/protocols/infra/date'
 
 export class DayjsAdapter implements VerifyDateIsPassed, VerifyDateIsCurrent {
   async isCurrent(date: string): Promise<boolean> {
-    return isSame(date)
+    const dateInFormatIso = new Date(date).toISOString()
+    return isSame(dateInFormatIso)
   }
 
   async isPassed(date: string): Promise<boolean> {
-    return isBefore(date)
+    const dateInFormatIso = new Date(date).toISOString()
+    return isBefore(dateInFormatIso)
   }
 }
