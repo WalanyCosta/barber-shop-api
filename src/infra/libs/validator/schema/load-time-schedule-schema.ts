ebf4036d7e7f47bc5c47666e43244420eecb991b
WalanyCosta <walanybnegro@gmail.com>
2024-08-09 16:17:38 +0100

    feat: ensure LoadTimeScheduleSchema returns error if barberId is empty

--- src/infra/libs/validator/schema/load-time-schedule-schema.ts
+++ src/infra/libs/validator/schema/load-time-schedule-schema.ts
@@ -1,8 +1,10 @@
 import * as z from 'zod'
 
 export const loadTimeScheduleSchema = z.object({
-  barberId: z.string({
-    required_error: 'barberId is required',
-    invalid_type_error: 'barberId type is invalid',
-  }),
+  barberId: z
+    .string({
+      required_error: 'barberId is required',
+      invalid_type_error: 'barberId type is invalid',
+    })
+    .min(1, { message: 'barberId is empty' }),
 })
