24687094b6f85f7b0295eaed160e03ab08462f9c
WalanyCosta <walanybnegro@gmail.com>
2024-08-09 12:15:30 +0100

    feat: ensure DayjsIsAdapter calls dayjs.isSame calls with correct date

--- src/infra/libs/date-adapter/config/instance-day-js.ts
+++ src/infra/libs/date-adapter/config/instance-day-js.ts
@@ -3,3 +3,7 @@ import dayjs from 'dayjs'
 export function isBefore(date: string): boolean {
   return dayjs().isAfter(date, 'date')
 }
+
+export function isSame(date: string): boolean {
+  return dayjs().isSame(date, 'date')
+}
--- src/infra/libs/date-adapter/day-js-adapter.ts
+++ src/infra/libs/date-adapter/day-js-adapter.ts
@@ -1,7 +1,12 @@
 import { type VerifyDateIsPassed } from '@/domain/protocols/infra/date/verify-date-is-passed'
-import { isBefore } from './config/instance-day-js'
+import { isBefore, isSame } from './config/instance-day-js'
+import { type VerifyDateIsCurrent } from '@/domain/protocols/infra/date'
 
-export class DayjsAdapter implements VerifyDateIsPassed {
+export class DayjsAdapter implements VerifyDateIsPassed, VerifyDateIsCurrent {
+  async isCurrent(date: string): Promise<boolean> {
+    return isSame(date)
+  }
+
   async isPassed(date: string): Promise<boolean> {
     return isBefore(date)
   }
